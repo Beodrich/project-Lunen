0a38929b759324f826d3578f6693af9c7dec541a
Alaxend0l <alaxend0l@gmail.com>
2020-05-10 11:07:02 -0500

    Test Scripts?
    Tried coding some stuff

---
+++ Project Lunen/Assets/Scripts/Monster.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster : MonoBehaviour
+{
+    struct Stat
+    {
+        int Base;
+        int Mod;
+        int Current;
+    }
+
+    private Stat Health;
+    private Stat Attack;
+    private Stat Defense;
+    private Stat Speed;
+
+    public int Level;
+}
