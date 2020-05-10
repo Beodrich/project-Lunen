a3c02beb9072aedad928e9db965dd4c4fc6d4705
Alaxend0l <alaxend0l@gmail.com>
2020-05-10 12:29:09 -0500

    Improving monster classes

--- Project Lunen/Assets/Scripts/Monster.cs
+++ Project Lunen/Assets/Scripts/Monster.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Monster : MonoBehaviour
 {
@@ -12,16 +13,49 @@ public class Monster : MonoBehaviour
         public int Current;
     }
 
+    public int Species;
+    public string Nickname;
+    public int Level;
+    public int NextXP;
+    public int CurrXP;
+
     public Stat Health;
     public Stat Attack;
     public Stat Defense;
     public Stat Speed;
 
-    public Move[] MoveSet;
+    public Action[] ActionSet;
 
-    public int Level;
+    
     
     public bool DEBUG;
 
-    public UnityEngine.UI.Text DEBUG_TEXT_OUTPUT;
+    public Text DEBUG_TEXT_OUTPUT;
+
+    void Start()
+    {
+        Attack.Current = Attack.Base + Attack.Mod;
+        Defense.Current = Defense.Base + Defense.Mod;
+        Speed.Current = Speed.Base + Speed.Mod;
+        if (DEBUG)
+        {
+            if (DEBUG_TEXT_OUTPUT != null)
+            {
+                string output = "Species: " + "!UNIMPLEMENTED!"
+                    + "\n" + "Nickname: " + Nickname + "\n"
+                    + StatInfo("Health", Health) + StatInfo("Attack", Attack) + StatInfo("Defense", Defense) + StatInfo("Speed", Speed);
+                DEBUG_TEXT_OUTPUT.GetComponent<Text>().text = output;
+            }
+        }
+    }
+
+    public string StatInfo(string name, Stat input)
+    {
+        string output =
+            name + ": " + "\n" +
+            "   BAS: " + input.Base + "\n" +
+            "   MOD: " + input.Mod + "\n" +
+            "   CUR: " + input.Current + "\n";
+        return output;
+    }
 }
