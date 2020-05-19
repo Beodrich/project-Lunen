3b4b26edf8ff3ee3ea77b32c23344d44fe0c19cc
Beodrich <60353906+Beodrich@users.noreply.github.com>
2020-05-19 13:03:41 -0500

    stuff

--- Assets/Scripts/UI_Shop.cs
+++ Assets/Scripts/UI_Shop.cs
@@ -1,7 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
+using TMPro;
 public class UI_Shop : MonoBehaviour
 {
     // Start is called before the first frame update
@@ -14,5 +15,25 @@ public class UI_Shop : MonoBehaviour
 
         shopItemTemp = container.Find("ShopItemTemp");
         shopItemTemp.gameObject.SetActive(false);
+    }
+    private void Start()
+    {
+        CreateItemButton()
+    }
+    private void CreateItemButton(Sprite itemSprint, string itemName, int itemCost, int positionIndex) {
+        Transform shopeItemTranform = Instantiate(shopItemTemp, container);
+      RectTransform shopItemTranform=  shopItemTemp.GetComponent<RectTransform>();
+        float shopItemHeight = 30f;
+        shopItemTranform.anchoredPosition = new Vector2(0, -shopItemHeight * positionIndex);
+        shopItemTranform.Find("nametext").GetComponent<TextMeshProUGUI>().SetText(itemName);
+
+        shopItemTranform.Find("PriceText").GetComponent<TextMeshProUGUI>().SetText(itemCost.ToString());
+        shopItemTranform.Find("itemPicture").GetComponent<Image>().sprite = itemSprint;
+        
+    
+    
+    
+    
+    
     }
 }
