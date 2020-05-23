aaad5ece23d3bd59939e66670cae15cb50a1745b
Beodrich <benodrich155@gmail.com>
2020-05-23 15:34:42 -0500

    shop stuff

--- Assets/Scripts/UI_Shop.cs
+++ Assets/Scripts/UI_Shop.cs
@@ -8,50 +8,56 @@ public class UI_Shop : MonoBehaviour
     // Start is called before the first frame update
     private Transform container;
     private Transform shopItemTemp;
-    private IShopCustomer customer;
+    private List<Item.ItemType> playerInventory = new List<Item.ItemType>();
+
+
+
+
     private void Awake()
     {
         container = transform.Find("container");
-
         shopItemTemp = container.Find("ShopItemTemp");
         shopItemTemp.gameObject.SetActive(false);
+
     }
     private void Start()
     {
-        CreateItemButton(Item.ItemType.Potion,Item.GetSprite(Item.ItemType.Potion), "Potion", Item.GetCost(Item.ItemType.Potion), 0);
-
-        CreateItemButton(Item.ItemType.Pokeball, Item.GetSprite(Item.ItemType.Pokeball), "PokeBall", Item.GetCost(Item.ItemType.Pokeball), 1);
+        CreateButton(Item.ItemType.Pokeball, Item.GetSprite(Item.ItemType.Pokeball), "PokeBall", Item.GetCost(Item.ItemType.Pokeball), 0);
+        CreateButton(Item.ItemType.Potion, Item.GetSprite(Item.ItemType.Potion), "Potion", Item.GetCost(Item.ItemType.Potion), 1);
         Hide();
     }
-    private void CreateItemButton(Item.ItemType itemType,Sprite itemSprint, string itemName, int itemCost, int positionIndex) {
-        Transform shopeItemTranform = Instantiate(shopItemTemp, container);
-      RectTransform shopItemTranform=  shopItemTemp.GetComponent<RectTransform>();
+    void CreateButton(Item.ItemType itemType, Sprite itemSprint, string itemName, int itemCost, int positionIndex)
+    {
+        Transform shopItemTransform = Instantiate(shopItemTemp, container);//make a copy of the item temp
+        RectTransform shopItemRectTransform = shopItemTransform.GetComponent<RectTransform>();
+        shopItemTransform.gameObject.SetActive(true);
         float shopItemHeight = 100f;
-        shopItemTranform.anchoredPosition = new Vector2(0, -shopItemHeight * positionIndex);
-        shopItemTranform.Find("nametext").GetComponent<TextMeshProUGUI>().SetText(itemName);
+        shopItemRectTransform.anchoredPosition = new Vector2(0, -shopItemHeight * positionIndex);
+        shopItemTransform.Find("PriceText").GetComponent<TextMeshProUGUI>().SetText(itemCost.ToString());
 
-        shopItemTranform.Find("PriceText").GetComponent<TextMeshProUGUI>().SetText(itemCost.ToString());
-        shopItemTranform.Find("itemPicture").GetComponent<Image>().sprite = itemSprint;
-
-
-        //button stuff
-        shopItemTranform.GetComponent<Button>().onClick.AddListener(() => TryBuyItem(itemType));//lamda function to call when the user clicks on the button
-      
-    
-    
-    
-    
-    
+        shopItemTransform.Find("nametext").GetComponent<TextMeshProUGUI>().SetText(itemName);
+        shopItemTransform.Find("itemPicture").GetComponent<Image>().sprite = itemSprint;
+        shopItemTransform.GetComponent<Button>().onClick.AddListener(() => TryBuyItem(itemType));
     }
     void TryBuyItem(Item.ItemType itemType) {
-        customer.BoughtItem(itemType);
+        BoughtItem(itemType);
     }
-    public void Show(IShopCustomer customer) {
-        this.customer = customer;
+   public void Show() {
         gameObject.SetActive(true);
     }
-    public void Hide() {
-        gameObject.SetActive(false);
-    
+    public void Hide() { gameObject.SetActive(false); }
+    void BoughtItem(Item.ItemType itemType) {
+      //  Debug.Log("Bought an item"+ itemType);
+        playerInventory.Add(itemType);
+        Debug.Log(playerInventory);
     }
 }
+
+
+
+
+
+
+
+
+
